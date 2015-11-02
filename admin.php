05abcd6e22edc3553f8e1550547ecef627596552
ahmetcankal <acankal@gmail.com>
2015-11-02 10:12:36 +0200

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+session_start();
+
+
+echo "Kullanıcı adınız:".$_SESSION['kulad']."<br>";
+echo "Parolanız:".$_SESSION['parola'];
+
+
+
+
+?>
+
